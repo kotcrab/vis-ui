767d10087646e7380583a3f161b7e73e4e34a311
kotcrab <kotcrab@gmail.com>
2014-04-15 20:34:32 +0200

    Removed unnecessary try-catch

--- src/main/java/pl/kotcrab/vis/sceneeditor/serializer/PreferencesSerializer.java
+++ src/main/java/pl/kotcrab/vis/sceneeditor/serializer/PreferencesSerializer.java
@@ -1,4 +1,3 @@
-
 package pl.kotcrab.vis.sceneeditor.serializer;
 
 import java.util.ArrayList;
@@ -7,42 +6,40 @@ import pl.kotcrab.vis.sceneeditor.SceneEditor;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Preferences;
-import com.badlogic.gdx.utils.SerializationException;
 
-public class PreferencesSerializer extends AbstractJsonSerializer {
+public class PreferencesSerializer extends AbstractJsonSerializer
+{
 	private static final String TAG = "VisSceneEditor:PreferencesSerializer";
-
+	
 	private Preferences prefs;
 	private String keyName;
-
-	public PreferencesSerializer (SceneEditor editor, Preferences prefs, String keyName) {
+	
+	public PreferencesSerializer(SceneEditor editor, Preferences prefs, String keyName)
+	{
 		super(editor);
 		this.prefs = prefs;
 		this.keyName = keyName;
 	}
-
+	
 	@Override
-	public boolean saveJsonData (ArrayList<ObjectInfo> infos) {
-		try {
-			prefs.putString(keyName, getJson().toJson(infos));
-			prefs.flush();
-			Gdx.app.log(TAG, "Saved changes to preferences.");
-			return true;
-		} catch (SerializationException e) {
-			Gdx.app.error(TAG, "Error while saving to preferences.");
-			e.printStackTrace();
-			return false;
-		}
+	public boolean saveJsonData(ArrayList<ObjectInfo> infos)
+	{
+		prefs.putString(keyName, getJson().toJson(infos));
+		prefs.flush();
+		Gdx.app.log(TAG, "Saved changes to preferences.");
+		return true;
 	}
-
+	
 	@SuppressWarnings("unchecked")
 	@Override
-	public ArrayList<ObjectInfo> loadJsonData () {
+	public ArrayList<ObjectInfo> loadJsonData()
+	{
 		return getJson().fromJson(new ArrayList<ObjectInfo>().getClass(), prefs.getString(keyName));
 	}
-
+	
 	@Override
-	public boolean isReadyToLoad () {
+	public boolean isReadyToLoad()
+	{
 		return prefs.contains(keyName);
 	}
 }
