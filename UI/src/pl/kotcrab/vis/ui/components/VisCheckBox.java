52509c617435513d25e6a1cb622c07d88191cd68
Paweł Pastuszak <kotcrab@gmail.com>
2014-10-29 21:45:13 +0100

    Removed polish diacritics chars

--- UI/src/pl/kotcrab/vis/ui/components/VisCheckBox.java
+++ UI/src/pl/kotcrab/vis/ui/components/VisCheckBox.java
@@ -35,7 +35,7 @@ import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 
 /** A checkbox is a button that contains an image indicating the checked or unchecked state and a label.
  * @author Nathan Sweet
- * @author Paweł Pastuszak */
+ * @author Pawel Pastuszak */
 @SuppressWarnings("rawtypes")
 public class VisCheckBox extends TextButton implements Focusable {
 	// This class were copied from LibGDX, few lines were changed.
