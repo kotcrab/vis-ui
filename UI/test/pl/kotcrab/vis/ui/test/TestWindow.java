b179c3c41ed33b9ed5cbdd1dc73f52b033d6d9aa
Paweł Pastuszak <kotcrab@gmail.com>
2014-10-30 23:01:02 +0100

    Updated test

--- UI/test/pl/kotcrab/vis/ui/test/TestWindow.java
+++ UI/test/pl/kotcrab/vis/ui/test/TestWindow.java
@@ -17,6 +17,7 @@
 package pl.kotcrab.vis.ui.test;
 
 import pl.kotcrab.vis.ui.TableUtils;
+import pl.kotcrab.vis.ui.VisTable;
 import pl.kotcrab.vis.ui.components.VisCheckBox;
 import pl.kotcrab.vis.ui.components.VisLabel;
 import pl.kotcrab.vis.ui.components.VisTextButton;
@@ -24,55 +25,62 @@ import pl.kotcrab.vis.ui.components.VisTextField;
 import pl.kotcrab.vis.ui.components.VisWindow;
 
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 public class TestWindow extends VisWindow {
 
 	public TestWindow (Stage parent) {
-		super(parent, "Test window");
+		super(parent, "test window");
 		setModal(true);
 
-		VisTextField projectRoot = new VisTextField();
-		TextButton chooseButton = new VisTextButton("Choose...");
-		VisTextField sourceLoc = new VisTextField("/core/src");
-		VisTextField assetsLoc = new VisTextField("/android/assets");
-
-		VisCheckBox signFiles = new VisCheckBox(" Sign files using private key");
-
 		TableUtils.setSpaceDefaults(this);
-
 		columnDefaults(0).left();
-		columnDefaults(1).width(300);
 
-		add(new VisLabel("Project root:"));
-		add(projectRoot);
-		add(chooseButton);
-		row();
+		// TODO textarea list selectbox progress bar slider scroll pane splitpane tree
 
-		add(new VisLabel("Source folder:"));
-		add(sourceLoc).fill();
-		row();
+		VisLabel label = new VisLabel("label");
 
-		add(new VisLabel("Assets folder:"));
-		add(assetsLoc).fill();
-		row();
+		VisTable labelTable = new VisTable(true);
+		labelTable.add(label);
+		// ---
 
-		add(signFiles).colspan(2);
-		row();
+		VisTextButton normalButton = new VisTextButton("button");
+		VisTextButton disabledButton = new VisTextButton("disabled");
+		disabledButton.setDisabled(true);
 
-		Table buttonTable = new Table();
-		TableUtils.setSpaceDefaults(buttonTable);
+		VisTable buttonTable = new VisTable(true);
+		buttonTable.add(normalButton);
+		buttonTable.add(disabledButton);
 
-		buttonTable.defaults().minWidth(70);
+		// ---
 
-		TextButton cancelButton = new VisTextButton("Cancel");
-		TextButton createButton = new VisTextButton("Create");
+		VisCheckBox normalCheckbox = new VisCheckBox(" checkbox");
+		VisCheckBox disabledCheckbox = new VisCheckBox(" disabled");
+		disabledCheckbox.setDisabled(true);
 
-		buttonTable.add(cancelButton);
-		buttonTable.add(createButton);
+		VisTable checkboxTable = new VisTable(true);
+		checkboxTable.add(normalCheckbox);
+		checkboxTable.add(disabledCheckbox);
 
-		add(buttonTable).colspan(3).right();
+		// ---
+
+		VisTextField normalTextfield = new VisTextField("textbox");
+		VisTextField disabledTextfield = new VisTextField("disabled");
+		VisTextField passwordTextfield = new VisTextField("password");
+		disabledTextfield.setDisabled(true);
+		passwordTextfield.setPasswordMode(true);
+		passwordTextfield.setPasswordCharacter('*');
+
+		VisTable textfieldTable = new VisTable(true);
+		textfieldTable.add(normalTextfield);
+		textfieldTable.add(disabledTextfield);
+		textfieldTable.add(passwordTextfield);
+
+		// ---
+
+		add(labelTable).row();
+		add(buttonTable).row();
+		add(checkboxTable).row();
+		add(textfieldTable).row();
 
 		pack();
 		setPositionToCenter();
