df5ea5c42386a3318b30f9d5de962ac349305e2c
Paweł Pastuszak <kotcrab@gmail.com>
2014-11-15 21:06:04 +0100

    Added markup-font

--- UI/src/pl/kotcrab/vis/ui/widget/VisImageButton.java
+++ UI/src/pl/kotcrab/vis/ui/widget/VisImageButton.java
@@ -27,8 +27,8 @@ import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Cell;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Scaling;
 
 public class VisImageButton extends Button implements Focusable {
