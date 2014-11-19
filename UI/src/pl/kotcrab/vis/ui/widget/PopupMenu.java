efa1fb5c341ffb3791236d783ebf9743175926ce
Paweł Pastuszak <kotcrab@gmail.com>
2014-11-19 22:18:08 +0100

    Added border to popup menu

--- UI/src/pl/kotcrab/vis/ui/widget/PopupMenu.java
+++ UI/src/pl/kotcrab/vis/ui/widget/PopupMenu.java
@@ -16,14 +16,59 @@
 
 package pl.kotcrab.vis.ui.widget;
 
+import pl.kotcrab.vis.ui.VisUI;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 
 public class PopupMenu extends Table {
+	private PopupMenuStyle style;
+	
 	public PopupMenu () {
+		style = VisUI.skin.get(PopupMenuStyle.class);
 	}
 
 	public void addItem (MenuItem item) {
 		add(item).fillX().row();
 		pack();
+	}
+	
+	@Override
+	public void draw(Batch batch, float parentAlpha)
+	{
+		super.draw(batch, parentAlpha);
+		style.border.draw(batch, getX(), getY(), getWidth(), getHeight());
+		
+	}
+
+	static public class PopupMenuStyle {
+		public Drawable border;
+
+		public PopupMenuStyle () {
+		}
+
+		public PopupMenuStyle (Drawable border) {
+			this.border = border;
+		}
+
+		public PopupMenuStyle (PopupMenuStyle style) {
+			this.border = style.border;
+		}
+
+//
+// public VisTextFieldStyle () {
+// }
+//
+// public VisTextFieldStyle (BitmapFont font, Color fontColor, Drawable cursor, Drawable selection, Drawable background) {
+// super(font, fontColor, cursor, selection, background);
+// }
+//
+// public VisTextFieldStyle (VisTextFieldStyle style) {
+// super(style);
+// this.focusBorder = style.focusBorder;
+// this.errorBorder = style.errorBorder;
+// this.backgroundOver = style.backgroundOver;
+// }
 	}
 }
