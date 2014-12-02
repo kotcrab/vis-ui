0613b889bf359c48140e9ace7c5a47e9ff67b46f
Paweł Pastuszak <kotcrab@gmail.com>
2014-12-02 23:06:01 +0100

    Added FileUtils.toFileHandle(File file)

--- UI/src/pl/kotcrab/vis/ui/widget/file/FileUtils.java
+++ UI/src/pl/kotcrab/vis/ui/widget/file/FileUtils.java
@@ -22,6 +22,7 @@ import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.Comparator;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.utils.Array;
 
@@ -81,5 +82,8 @@ public class FileUtils {
 	public static boolean isUnix () {
 		return (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0);
 	}
-
+	
+	public static FileHandle toFileHandle (File file) {
+		return Gdx.files.absolute(file.getAbsolutePath());
+	}
 }
