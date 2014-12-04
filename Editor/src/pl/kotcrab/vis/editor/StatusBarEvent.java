823178505af520445aae2641e32300ec592e043a
Paweł Pastuszak <kotcrab@gmail.com>
2014-12-04 22:41:36 +0100

    Added StatusBar

---
+++ Editor/src/pl/kotcrab/vis/editor/StatusBarEvent.java
@@ -0,0 +1,14 @@
+
+package pl.kotcrab.vis.editor;
+
+import pl.kotcrab.utils.event.Event;
+
+public class StatusBarEvent implements Event {
+	public String text;
+	public int timeSeconds;
+
+	public StatusBarEvent (String text, int timeSeconds) {
+		this.text = text;
+		this.timeSeconds = timeSeconds;
+	}
+}
---
+++ Editor/src/pl/kotcrab/vis/editor/module/StatusBar.java
@@ -0,0 +1,74 @@
+
+package pl.kotcrab.vis.editor.module;
+
+import pl.kotcrab.utils.event.Event;
+import pl.kotcrab.utils.event.EventListener;
+import pl.kotcrab.vis.editor.App;
+import pl.kotcrab.vis.editor.Editor;
+import pl.kotcrab.vis.editor.StatusBarEvent;
+import pl.kotcrab.vis.ui.VisTable;
+import pl.kotcrab.vis.ui.VisUI;
+import pl.kotcrab.vis.ui.widget.VisLabel;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Timer;
+import com.badlogic.gdx.utils.Timer.Task;
+
+public class StatusBar extends ModuleAdapter implements EventListener {
+	public VisTable table;
+	public VisLabel statusLabel;
+
+	public Timer timer;
+
+	public StatusBar () {
+		timer = new Timer();
+
+		statusLabel = new VisLabel("Ready");
+
+		table = new VisTable();
+		table.setBackground(VisUI.skin.getDrawable("button"));
+		table.add(statusLabel);
+		table.add(new Image(VisUI.skin.getRegion("button"))).expand().fill();
+
+	}
+
+	public void addToStage (Table root) {
+		root.add(table).fillX().expandX();
+	}
+
+	public void setText (String newText, int timeSeconds) {
+		statusLabel.setText(newText);
+		timer.clear();
+		timer.scheduleTask(resetTask, timeSeconds);
+	}
+
+	@Override
+	public void added () {
+		App.eventBus.register(this);
+		addToStage(Editor.instance.getRoot());
+	}
+
+	@Override
+	public void dispose () {
+		App.eventBus.unregister(this);
+	}
+
+	private Task resetTask = new Task() {
+		@Override
+		public void run () {
+			statusLabel.setText("Ready");
+		}
+	};
+
+	@Override
+	public boolean onEvent (Event e) {
+		if (e instanceof StatusBarEvent) {
+			StatusBarEvent event = (StatusBarEvent)e;
+			setText(event.text, event.timeSeconds);
+			return true;
+		}
+
+		return false;
+	}
+}
