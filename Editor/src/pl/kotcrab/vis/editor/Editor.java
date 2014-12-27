e307f662579bdaee1bc04f0b3bc43d5eb82e9008
Paweł Pastuszak <kotcrab@gmail.com>
2014-12-27 17:24:16 +0100

    Load missing module

--- Editor/src/pl/kotcrab/vis/editor/Editor.java
+++ Editor/src/pl/kotcrab/vis/editor/Editor.java
@@ -37,6 +37,7 @@ import pl.kotcrab.vis.editor.module.project.Project;
 import pl.kotcrab.vis.editor.module.project.ProjectInfoTabModule;
 import pl.kotcrab.vis.editor.module.project.ProjectModuleContainer;
 import pl.kotcrab.vis.editor.module.scene.SceneIOModule;
+import pl.kotcrab.vis.editor.module.scene.SceneTabsControllerModule;
 import pl.kotcrab.vis.editor.ui.EditorFrame;
 import pl.kotcrab.vis.editor.ui.tab.Tab;
 import pl.kotcrab.vis.editor.ui.tab.TabViewMode;
@@ -205,6 +206,7 @@ public class Editor extends ApplicationAdapter implements EventListener {
 		projectModuleContainer.setProject(project);
 
 		projectModuleContainer.add(new FileAccessModule());
+		projectModuleContainer.add(new SceneTabsControllerModule());
 		projectModuleContainer.add(new SceneIOModule());
 		projectModuleContainer.add(new ProjectInfoTabModule());
 		projectModuleContainer.add(new AssetsManagerUIModule());
