57255cd52e535089b2ba091496001a56f35c7005
Paweł Pastuszak <kotcrab@gmail.com>
2015-01-17 12:10:34 +0100

    ProcessingQueue refactor

--- Editor/src/com/kotcrab/vis/editor/util/ProcessingQueue.java
+++ Editor/src/com/kotcrab/vis/editor/util/ProcessingQueue.java
@@ -21,13 +21,13 @@ package com.kotcrab.vis.editor.util;
 
 import java.util.concurrent.ArrayBlockingQueue;
 
-/** Blocking queue that allows processing objects of any type, one by one. Element are processed in different thread. This queue
+/**
+ * Blocking queue that allows processing objects of any type, one by one. Element are processed in different thread. This queue
  * orders elements FIFO. Uses {@link ArrayBlockingQueue}. If not started in daemon mode and no longer needed
  * {@link ProcessingQueue#stop()} must be called to shutdown processing thread.
- * 
+ * @param <E> type of objects that will be processed
  * @author Pawel Pastuszak
- *
- * @param <E> type of objects that will be processed */
+ */
 public abstract class ProcessingQueue<E> {
 	private boolean running = true;
 
@@ -39,21 +39,23 @@ public abstract class ProcessingQueue<E> {
 
 	private ArrayBlockingQueue<E> queue;
 
-	/** Creates {@link ProcessingQueue}.
-	 * 
+	/**
+	 * Creates {@link ProcessingQueue}.
 	 * @param threadName name of processing thread that will be created
-	 * @param capacity queue capacity, if queue is full, {@link ProcessingQueue#processLater()} will block until there is space in
-	 *           queue */
+	 * @param capacity   queue capacity, if queue is full, {@link ProcessingQueue#processLater(E element)} will block until there is space in
+	 *                   queue
+	 */
 	public ProcessingQueue (String threadName, int capacity) {
 		this.threadName = threadName;
 		this.capacity = capacity;
 		start();
 	}
 
-	/** Creates {@link ProcessingQueue} with fixed 256 objects capacity. If queue is full, {@link ProcessingQueue#processLater()}
+	/**
+	 * Creates {@link ProcessingQueue} with fixed 256 objects capacity. If queue is full, {@link ProcessingQueue#processLater(E element)}
 	 * will block until there is space in queue
-	 * 
-	 * @param threadName name of processing thread that will be created */
+	 * @param threadName name of processing thread that will be created
+	 */
 	public ProcessingQueue (String threadName) {
 		this.threadName = threadName;
 		start();
@@ -66,7 +68,7 @@ public abstract class ProcessingQueue<E> {
 	}
 
 	private void start () {
-		queue = new ArrayBlockingQueue<E>(capacity);
+		queue = new ArrayBlockingQueue<>(capacity);
 
 		processingThread = new Thread(new Runnable() {
 			@Override
@@ -93,8 +95,10 @@ public abstract class ProcessingQueue<E> {
 		queue.clear();
 	}
 
-	/** Add element to queue. If queue is full, execution will be blocked until there is empty space in queue.
-	 * @param element to be added to queue */
+	/**
+	 * Add element to queue. If queue is full, execution will be blocked until there is empty space in queue.
+	 * @param element to be added to queue
+	 */
 	public void processLater (E element) {
 		try {
 			queue.put(element);
@@ -103,7 +107,9 @@ public abstract class ProcessingQueue<E> {
 		}
 	}
 
-	/** Called by processing thread when queue element should be processed.
-	 * @param element to be processed */
+	/**
+	 * Called by processing thread when queue element should be processed.
+	 * @param element to be processed
+	 */
 	protected abstract void processQueueElement (E element);
 }
