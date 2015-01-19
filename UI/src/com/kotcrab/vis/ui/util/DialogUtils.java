eadcc4150fd9f4cb944247a2fa87e85d4ce55438
Paweł Pastuszak <kotcrab@gmail.com>
2015-01-19 23:29:12 +0100

    Added showInputDialog in DialogUtils

--- UI/src/com/kotcrab/vis/ui/util/DialogUtils.java
+++ UI/src/com/kotcrab/vis/ui/util/DialogUtils.java
@@ -17,15 +17,16 @@
 package com.kotcrab.vis.ui.util;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Cell;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.kotcrab.vis.ui.InputValidator;
 import com.kotcrab.vis.ui.VisTable;
-import com.kotcrab.vis.ui.widget.VisDialog;
-import com.kotcrab.vis.ui.widget.VisLabel;
-import com.kotcrab.vis.ui.widget.VisScrollPane;
-import com.kotcrab.vis.ui.widget.VisTextButton;
+import com.kotcrab.vis.ui.widget.*;
 
 public class DialogUtils {
 	public static void showOKDialog (Stage stage, String title, String text) {
@@ -37,13 +38,21 @@ public class DialogUtils {
 		stage.addActor(dialog.fadeIn());
 	}
 
+	public static void showInputDialog (Stage stage, String title, String fieldTitle, InputDialogListener listener) {
+		new InputDialog(stage, title, fieldTitle, listener);
+	}
+
+	public static void showInputDialog (Stage stage, String title, String fieldTitle, InputValidator validator, InputDialogListener listener) {
+		new InputDialog(stage, title, fieldTitle, validator, listener);
+	}
+
 	public static void showErrorDialog (Stage stage, String text) {
-		showErrorDialog(stage, text, (String)null);
+		showErrorDialog(stage, text, (String) null);
 	}
 
 	public static void showErrorDialog (Stage stage, String text, Exception exception) {
 		if (exception == null)
-			showErrorDialog(stage, text, (String)null);
+			showErrorDialog(stage, text, (String) null);
 		else
 			showErrorDialog(stage, text, getStackTrace(exception));
 	}
@@ -51,6 +60,101 @@ public class DialogUtils {
 	public static void showErrorDialog (Stage stage, String text, String details) {
 		ErrorDialog dialog = new ErrorDialog(text, details);
 		stage.addActor(dialog.fadeIn());
+	}
+
+	private static VisScrollPane createScrollPane (Actor widget) {
+		VisScrollPane scrollPane = new VisScrollPane(widget);
+		scrollPane.setOverscroll(false, true);
+		scrollPane.setFadeScrollBars(false);
+		return scrollPane;
+	}
+
+	private static String getStackTrace (Throwable throwable) {
+		StringBuilder builder = new StringBuilder();
+
+		for (StackTraceElement element : throwable.getStackTrace()) {
+			builder.append(element);
+			builder.append("\n");
+		}
+
+		return builder.toString();
+	}
+
+	public interface InputDialogListener {
+		public void finished (String input);
+	}
+
+	private static class InputDialog extends VisWindow {
+		private InputDialogListener listener;
+		private VisTextField field;
+		private VisTextButton okButton;
+
+		public InputDialog (Stage stage, String title, String fieldTitle, InputDialogListener listener) {
+			this(stage, title, fieldTitle, null, listener);
+		}
+
+		public InputDialog (Stage stage, String title, String fieldTitle, InputValidator validator, InputDialogListener listener) {
+			super(title);
+			this.listener = listener;
+
+			if (validator == null)
+				field = new VisTextField();
+			else
+				field = new VisValidableTextField(validator);
+
+			setModal(true);
+			add(new VisLabel(fieldTitle)).spaceRight(6).spaceBottom(3);
+			add(field).padRight(3).spaceBottom(3);
+			row();
+			add(okButton = new VisTextButton("OK")).colspan(2).padBottom(2);
+
+			pack();
+			centerWindow();
+
+			addListeners();
+
+			if (validator != null) {
+				addValidableFieldListener(field);
+				okButton.setDisabled(!field.isInputValid());
+			}
+
+			stage.addActor(fadeIn());
+			field.focusField();
+		}
+
+		private void addValidableFieldListener (final VisTextField field) {
+			field.addListener(new ChangeListener() {
+				@Override
+				public void changed (ChangeEvent event, Actor actor) {
+					if (field.isInputValid())
+						okButton.setDisabled(false);
+					else
+						okButton.setDisabled(true);
+				}
+			});
+		}
+
+		private void addListeners () {
+			okButton.addListener(new ChangeListener() {
+				@Override
+				public void changed (ChangeEvent event, Actor actor) {
+					listener.finished(field.getText());
+					fadeOut();
+				}
+			});
+
+			field.addListener(new InputListener() {
+				@Override
+				public boolean keyDown (InputEvent event, int keycode) {
+					if (keycode == Keys.ENTER && okButton.isDisabled() == false) {
+						listener.finished(field.getText());
+						fadeOut();
+					}
+
+					return super.keyDown(event, keycode);
+				}
+			});
+		}
 	}
 
 	private static class ErrorDialog extends VisDialog {
@@ -98,7 +202,7 @@ public class DialogUtils {
 
 		@Override
 		protected void result (Object object) {
-			int result = (Integer)object;
+			int result = (Integer) object;
 
 			if (result == DETAILS) {
 				detailsCell.setActor(detailsCell.hasActor() ? null : detailsTable);
@@ -109,21 +213,4 @@ public class DialogUtils {
 		}
 	}
 
-	private static VisScrollPane createScrollPane (Actor widget) {
-		VisScrollPane scrollPane = new VisScrollPane(widget);
-		scrollPane.setOverscroll(false, true);
-		scrollPane.setFadeScrollBars(false);
-		return scrollPane;
-	}
-
-	private static String getStackTrace (Throwable throwable) {
-		StringBuilder builder = new StringBuilder();
-
-		for (StackTraceElement element : throwable.getStackTrace()) {
-			builder.append(element);
-			builder.append("\n");
-		}
-		
-		return builder.toString();
-	}
 }
