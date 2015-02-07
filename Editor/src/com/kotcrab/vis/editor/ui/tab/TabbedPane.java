92bf7bd580f909459ecde45cea015894046bfd74
Paweł Pastuszak <kotcrab@gmail.com>
2015-02-07 18:07:59 +0100

    Refactored TabbedPane, now suports closeable

--- Editor/src/com/kotcrab/vis/editor/ui/tab/TabbedPane.java
+++ Editor/src/com/kotcrab/vis/editor/ui/tab/TabbedPane.java
@@ -19,24 +19,35 @@
 
 package com.kotcrab.vis.editor.ui.tab;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.ButtonGroup;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ObjectMap;
+import com.badlogic.gdx.utils.Scaling;
 import com.kotcrab.vis.ui.VisTable;
 import com.kotcrab.vis.ui.VisUI;
+import com.kotcrab.vis.ui.widget.VisImageButton;
+import com.kotcrab.vis.ui.widget.VisImageButton.VisImageButtonStyle;
 import com.kotcrab.vis.ui.widget.VisTextButton;
+import com.kotcrab.vis.ui.widget.VisTextButton.VisTextButtonStyle;
 
 public class TabbedPane {
+	private static final Drawable highlightBg = VisUI.skin.getDrawable("list-selection");
+
 	private VisTable tabsTable;
 	private VisTable mainTable;
 
 	private Array<Tab> tabs;
-	private ObjectMap<Tab, VisTextButton> tabsButtonMap;
+	private ObjectMap<Tab, TabButtonTable> tabsButtonMap;
 	private ButtonGroup<Button> group;
 
 	private Tab activeTab;
@@ -57,18 +68,18 @@ public class TabbedPane {
 
 		mainTable.add(tabsTable).padTop(2).left().expand();
 		mainTable.row();
+
 		// if height is not set bottomBar may sometimes disappear for some reason
-		mainTable.add(new Image(VisUI.skin.getDrawable("list-selection"))).expand().fill().height(1);
+		mainTable.add(new Image(highlightBg)).expand().fill().height(1);
 		mainTable.setBackground(VisUI.skin.getDrawable("menu-bg"));
 	}
 
 	public void add (Tab tab) {
 		tab.setPane(this);
 		tabs.add(tab);
-		if (activeTab != null) activeTab.onHide();
-		activeTab = tab;
 
 		rebuildTabsTable();
+		switchTab(tab);
 	}
 
 	public void add (int index, Tab tab) {
@@ -78,16 +89,18 @@ public class TabbedPane {
 	}
 
 	public boolean remove (Tab tab) {
+		int index = tabs.indexOf(tab, true);
 		boolean success = tabs.removeValue(tab, true);
 
 		if (success) {
 			tab.setPane(null);
-			rebuildTabsTable();
 			notifyListenersRemoved(tab);
 
 			if (tabs.size == 0)
 				notifyListenersRemovedAll();
-			else if (activeTab == tab) switchTab(0);
+			else if (activeTab == tab && index != 0) switchTab(--index);
+
+			rebuildTabsTable();
 		}
 
 		return success;
@@ -104,48 +117,37 @@ public class TabbedPane {
 	}
 
 	public void switchTab (int index) {
-		group.getButtons().get(index).setChecked(true);
+		tabsButtonMap.get(tabs.get(index)).select();
 	}
 
 	public void switchTab (Tab tab) {
-		tabsButtonMap.get(tab).setChecked(true);
+		tabsButtonMap.get(tab).select();
 	}
 
 	public void updateTabTitle (Tab tab) {
-		tabsButtonMap.get(tab).setText(tab.getButtonText());
+		tabsButtonMap.get(tab).button.setText(tab.getButtonText());
 	}
 
 	private void rebuildTabsTable () {
+		Tab lastSelectedTab = activeTab;
 		tabsTable.clear();
 		group.clear();
 		tabsButtonMap.clear();
 
 		for (final Tab tab : tabs) {
-			final VisTextButton button = new VisTextButton(tab.getButtonText(), "toggle");
-			button.setFocusBorderEnabled(false);
+			final TabButtonTable buttonTable = new TabButtonTable(tab);
 
-			tabsTable.add(button);
-			group.add(button);
-			tabsButtonMap.put(tab, button);
-
-			button.addListener(new ChangeListener() {
-				@Override
-				public void changed (ChangeEvent event, Actor actor) {
-					if (button.isChecked()) {
-						if (activeTab != null) activeTab.onHide();
-						activeTab = tab;
-						notifyListenersSwitched(tab);
-						tab.onShow();
-					}
-				}
-			});
+			tabsTable.add(buttonTable);
+			tabsButtonMap.put(tab, buttonTable);
+			group.add(buttonTable.button); //this will change activeTab
 
 			if (tabs.size == 1) {
-				button.setChecked(true);
+				buttonTable.select();
 				notifyListenersSwitched(tab);
 			}
 
-			if (tab == activeTab) button.setChecked(true); // maintains current previous tab while rebuilding
+			if (tab == lastSelectedTab)
+				buttonTable.select(); // maintains current previous tab while rebuilding
 		}
 	}
 
@@ -174,5 +176,97 @@ public class TabbedPane {
 	private void notifyListenersRemovedAll () {
 		for (TabbedPaneListener listener : listeners)
 			listener.removedAllTabs();
+	}
+
+	private class TabButtonTable extends VisTable {
+		public VisTextButton button;
+		public VisImageButton closeButton;
+		private Tab tab;
+
+		private VisTextButtonStyle buttonStyle;
+		private VisImageButtonStyle closeButtonStyle;
+
+		public TabButtonTable (final Tab tab) {
+			this.tab = tab;
+			button = new VisTextButton(tab.getButtonText(), "toggle");
+			button.setFocusBorderEnabled(false);
+
+			closeButton = new VisImageButton("close");
+			closeButton.getImage().setScaling(Scaling.fill);
+			closeButton.getImage().setColor(Color.RED);
+
+			addListeners();
+
+			buttonStyle = (VisTextButtonStyle) button.getStyle();
+			closeButtonStyle = closeButton.getStyle();
+
+			add(button);
+			if (tab.isCloseableByUser()) add(closeButton).size(14, button.getHeight());
+		}
+
+		private void addListeners () {
+			closeButton.addListener(new ChangeListener() {
+				@Override
+				public void changed (ChangeEvent event, Actor actor) {
+					TabbedPane.this.remove(tab);
+				}
+			});
+
+			button.addListener(new InputListener() {
+				@Override
+				public boolean touchDown (InputEvent event, float x, float y, int pointer, int mouseButton) {
+					closeButtonStyle.up = buttonStyle.down;
+					return false;
+				}
+
+				@Override
+				public boolean mouseMoved (InputEvent event, float x, float y) {
+					if (activeTab != tab)
+						closeButtonStyle.up = buttonStyle.over;
+					return false;
+				}
+
+				@Override
+				public void exit (InputEvent event, float x, float y, int pointer, Actor toActor) {
+					if (activeTab != tab)
+						closeButtonStyle.up = buttonStyle.up;
+				}
+
+				@Override
+				public void enter (InputEvent event, float x, float y, int pointer, Actor fromActor) {
+					if (activeTab != tab && Gdx.input.justTouched() == false)
+						closeButtonStyle.up = buttonStyle.over;
+				}
+			});
+
+			button.addListener(new ChangeListener() {
+				@Override
+				public void changed (ChangeEvent event, Actor actor) {
+					if (button.isChecked()) {
+						if (activeTab != null) {
+							TabButtonTable table = tabsButtonMap.get(activeTab);
+							if (table != null) table.deselect();
+						}
+
+
+						if (activeTab != null) activeTab.onHide();
+						activeTab = tab;
+						notifyListenersSwitched(tab);
+						tab.onShow();
+						closeButtonStyle.up = buttonStyle.down;
+						closeButtonStyle.over = null;
+					}
+				}
+			});
+		}
+
+		public void select () {
+			button.setChecked(true);
+		}
+
+		public void deselect () {
+			closeButtonStyle.up = buttonStyle.up;
+			closeButtonStyle.over = buttonStyle.over;
+		}
 	}
 }
