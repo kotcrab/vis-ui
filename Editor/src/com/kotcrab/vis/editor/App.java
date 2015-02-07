9a94bd629ab7ddfc5c22a0385aaefca7a6670260
Paweł Pastuszak <kotcrab@gmail.com>
2015-02-07 13:44:17 +0100

    OpenGL crash exit message

--- Editor/src/com/kotcrab/vis/editor/App.java
+++ Editor/src/com/kotcrab/vis/editor/App.java
@@ -37,6 +37,7 @@ public class App {
 	public static final String VERSION = "0.0.2-SNAPSHOT";
 
 	public static final boolean ERROR_REPORTS = false;
+	public static final boolean OPENGL_CRASH_BEFORE_EXIT_MESSAGE = true;
 	public static final boolean SNAPSHOT = VERSION.contains("SNAPSHOT");
 
 	public static final String JAR_FOLDER_PATH = getJarPath();
