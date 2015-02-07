7ae78377b247d22627fa9c7cd49889080db2abd7
Paweł Pastuszak <kotcrab@gmail.com>
2015-02-07 11:09:23 +0100

    Formated

--- UI/src/com/kotcrab/vis/ui/VisUI.java
+++ UI/src/com/kotcrab/vis/ui/VisUI.java
@@ -23,42 +23,41 @@ import com.badlogic.gdx.scenes.scene2d.utils.Align;
 
 /**
  * Makes loading VisUI skin easier
- *
  * @author Pawel Pastuszak
  */
 public class VisUI {
-	 private static int defaultTitleAlign = Align.left;
-	 public static Skin skin;
+	private static int defaultTitleAlign = Align.left;
+	//TODO make this private in 0.6.0
+	public static Skin skin;
 
-	 /** Loads default VisUI skin from library */
-	 public static void load () {
-		  load(Gdx.files.internal("com/kotcrab/vis/ui/uiskin.json"));
-	 }
+	/** Loads default VisUI skin from library */
+	public static void load () {
+		load(Gdx.files.internal("com/kotcrab/vis/ui/uiskin.json"));
+	}
 
-	 /** Loads provided skin, skin must be compatible with default VisUI skin */
-	 public static void load (FileHandle visSkinFile) {
-		  skin = new Skin(visSkinFile);
-	 }
+	/** Loads provided skin, skin must be compatible with default VisUI skin */
+	public static void load (FileHandle visSkinFile) {
+		skin = new Skin(visSkinFile);
+	}
 
-	 /** Unloads skin */
-	 public static void dispose () {
-		  if (skin != null) {
-				skin.dispose();
-				skin = null;
-		  }
-	 }
+	/** Unloads skin */
+	public static void dispose () {
+		if (skin != null) {
+			skin.dispose();
+			skin = null;
+		}
+	}
 
-	 /**
-	  * Sets default title align user for VisWindow and VisDialog
-	  *
-	  * @param defaultTitleAlign int value from {@link Align}
-	  */
-	 public static void setDefaultTitleAlign (int defaultTitleAlign) {
-		  VisUI.defaultTitleAlign = defaultTitleAlign;
-	 }
+	/** @return int value from {@link Align} */
+	public static int getDefaultTitleAlign () {
+		return defaultTitleAlign;
+	}
 
-	 /** @return int value from {@link Align} */
-	 public static int getDefaultTitleAlign () {
-		  return defaultTitleAlign;
-	 }
+	/**
+	 * Sets default title align user for VisWindow and VisDialog
+	 * @param defaultTitleAlign int value from {@link Align}
+	 */
+	public static void setDefaultTitleAlign (int defaultTitleAlign) {
+		VisUI.defaultTitleAlign = defaultTitleAlign;
+	}
 }
