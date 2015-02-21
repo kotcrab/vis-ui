e5910076072387da1e83dda18e87f30ee709574f
Paweł Pastuszak <kotcrab@gmail.com>
2015-02-21 23:19:36 +0100

    VisUI next verison changed to 0.7.0

--- UI/src/com/kotcrab/vis/ui/VisUI.java
+++ UI/src/com/kotcrab/vis/ui/VisUI.java
@@ -26,7 +26,7 @@ import com.badlogic.gdx.scenes.scene2d.utils.Align;
  * @author Pawel Pastuszak
  */
 public class VisUI {
-	public static final String VERSION = "0.6.2";
+	public static final String VERSION = "0.7.0";
 
 	private static int defaultTitleAlign = Align.left;
 	private static Skin skin;
