e99d2ae0936815fe25e08bdb8a7b28ec222f000f
Paweł Pastuszak <kotcrab@gmail.com>
2015-03-07 17:56:35 +0100

    Added Nullables

--- UI/src/com/kotcrab/vis/ui/util/Nullables.java
+++ UI/src/com/kotcrab/vis/ui/util/Nullables.java
@@ -16,10 +16,97 @@
 
 package com.kotcrab.vis.ui.util;
 
-import com.kotcrab.vis.ui.building.utilities.Padding;
-
+/**
+ * Provides static utilities for nullable objects to avoid NullPointerExceptions. Java 6 compatible, although
+ * some methods might be quite useful for lambdas.
+ * @author MJ
+ */
 public class Nullables {
-	public static Padding getOrElse (Padding padding, Padding defaultWidgetPadding) {
-		return padding != null ? padding : defaultWidgetPadding;
+	private Nullables () {
+	}
+
+	/** A simple null-check. */
+	public static boolean isNull (final Object nullable) {
+		return nullable == null;
+	}
+
+	/** A simple not-null-check. */
+	public static boolean isNotNull (final Object nullable) {
+		return nullable != null;
+	}
+
+	/**
+	 * @param nullable probable null.
+	 * @param alternative will be return if nullable is null.
+	 */
+	public static <Type> Type getOrElse (final Type nullable, final Type alternative) {
+		return nullable == null ? alternative : nullable;
+	}
+
+	/**
+	 * @param nullable probable null.
+	 * @param command will be executed only if nullable object exists.
+	 */
+	public static void executeIfNotNull (final Object nullable, final Runnable command) {
+		if (nullable != null) {
+			command.run();
+		}
+	}
+
+	/** @return true if objects are equal (using equals method) or if both are null. */
+	public static boolean areEqual (final Object first, final Object second) {
+		return first == second || first != null && first.equals(second);
+	}
+
+	/**
+	 * @param nullables nullable objects.
+	 * @return true if any of the objects is null.
+	 */
+	public static boolean isAnyNull (final Object... nullables) {
+		for (final Object object : nullables) {
+			if (object == null) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * @param nullables nullable objects.
+	 * @return true if all passed objects are null.
+	 */
+	public static boolean areAllNull (final Object... nullables) {
+		for (final Object object : nullables) {
+			if (object != null) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * @param nullables nullable objects.
+	 * @return true if any of the objects is not null.
+	 */
+	public static boolean isAnyNotNull (final Object... nullables) {
+		for (final Object object : nullables) {
+			if (object != null) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * @param nullables nullable objects.
+	 * @return true if all passed objects are not null.
+	 */
+	public static boolean areAllNotNull (final Object... nullables) {
+		for (final Object object : nullables) {
+			if (object == null) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
