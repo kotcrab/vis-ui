65ebd4ab8cdc2c6fb8ce581bb481f4ddd6101f82
Paweł Pastuszak <kotcrab@gmail.com>
2015-03-11 22:40:00 +0100

    Use new keepWithinStage

--- UI/src/com/kotcrab/vis/ui/widget/Tooltip.java
+++ UI/src/com/kotcrab/vis/ui/widget/Tooltip.java
@@ -16,6 +16,8 @@
 
 package com.kotcrab.vis.ui.widget;
 
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
@@ -142,6 +144,31 @@ public class Tooltip extends VisTable {
 		super.setPosition((int) x, (int) y);
 	}
 
+	private void keepWithinStage () {
+		Stage stage = getStage();
+		Camera camera = stage.getCamera();
+		if (camera instanceof OrthographicCamera) {
+			OrthographicCamera orthographicCamera = (OrthographicCamera) camera;
+			float parentWidth = stage.getWidth();
+			float parentHeight = stage.getHeight();
+			if (getX(Align.right) - camera.position.x > parentWidth / 2 / orthographicCamera.zoom)
+				setPosition(camera.position.x + parentWidth / 2 / orthographicCamera.zoom, getY(Align.right), Align.right);
+			if (getX(Align.left) - camera.position.x < -parentWidth / 2 / orthographicCamera.zoom)
+				setPosition(camera.position.x - parentWidth / 2 / orthographicCamera.zoom, getY(Align.left), Align.left);
+			if (getY(Align.top) - camera.position.y > parentHeight / 2 / orthographicCamera.zoom)
+				setPosition(getX(Align.top), camera.position.y + parentHeight / 2 / orthographicCamera.zoom, Align.top);
+			if (getY(Align.bottom) - camera.position.y < -parentHeight / 2 / orthographicCamera.zoom)
+				setPosition(getX(Align.bottom), camera.position.y - parentHeight / 2 / orthographicCamera.zoom, Align.bottom);
+		} else if (getParent() == stage.getRoot()) {
+			float parentWidth = stage.getWidth();
+			float parentHeight = stage.getHeight();
+			if (getX() < 0) setX(0);
+			if (getRight() > parentWidth) setX(parentWidth - getWidth());
+			if (getY() < 0) setY(0);
+			if (getTop() > parentHeight) setY(parentHeight - getHeight());
+		}
+	}
+
 	private class DisplayTask extends Task {
 		@Override
 		public void run () {
@@ -180,18 +207,6 @@ public class Tooltip extends VisTable {
 				fadeOut();
 
 			return false;
-		}
-	}
-
-	void keepWithinStage () {
-		Stage stage = getStage();
-		if (getParent() == stage.getRoot()) {
-			float parentWidth = stage.getWidth();
-			float parentHeight = stage.getHeight();
-			if (getX() < 0) setX(0);
-			if (getRight() > parentWidth) setX(parentWidth - getWidth());
-			if (getY() < 0) setY(0);
-			if (getTop() > parentHeight) setY(parentHeight - getHeight());
 		}
 	}
 }
