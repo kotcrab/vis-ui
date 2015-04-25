891206f7fde00ffd9657d4e5700446c6b280980f
Paweł Pastuszak <kotcrab@gmail.com>
2015-04-25 12:07:28 +0200

    Releasing VisUI 0.7.3, preparing for next version dev

--- UI/src/com/kotcrab/vis/ui/VisUI.java
+++ UI/src/com/kotcrab/vis/ui/VisUI.java
@@ -30,7 +30,7 @@ import java.util.Locale;
  */
 public class VisUI {
 	/** Current VisUI version, does not include SNAPSHOT even if this version is snapshot */
-	public static final String VERSION = "0.7.3";
+	public static final String VERSION = "0.7.4";
 
 	private static int defaultTitleAlign = Align.left;
 	private static Skin skin;
