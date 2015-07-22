cd24cee5aa20ebebe65f083093301484a1bd8b25
Paweł Pastuszak <kotcrab@gmail.com>
2015-07-22 17:16:24 +0200

    Added MoveEntitiesAction

---
+++ Editor/src/com/kotcrab/vis/editor/module/scene/action/MoveEntitiesAction.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2014-2015 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.kotcrab.vis.editor.module.scene.action;
+
+import com.badlogic.gdx.utils.Array;
+import com.kotcrab.vis.editor.proxy.EntityProxy;
+import com.kotcrab.vis.editor.util.undo.UndoableAction;
+
+public class MoveEntitiesAction implements UndoableAction {
+	private Array<EntityPositionData> oldDatas = new Array<>();
+	private Array<EntityPositionData> newDatas = new Array<>();
+	private Array<EntityProxy> entities = new Array<>();
+
+	public MoveEntitiesAction (Array<EntityProxy> entities) {
+		this.entities = new Array<>(entities);
+
+		for (EntityProxy entity : entities) {
+			EntityPositionData positionData = new EntityPositionData();
+			positionData.saveFrom(entity);
+			oldDatas.add(positionData);
+		}
+	}
+
+	public void saveNewData () {
+		for (EntityProxy entity : entities) {
+			EntityPositionData positionData = new EntityPositionData();
+			positionData.saveFrom(entity);
+			newDatas.add(positionData);
+		}
+	}
+
+	@Override
+	public void execute () {
+		entities.forEach(EntityProxy::reload);
+
+		for (int i = 0; i < entities.size; i++) {
+			newDatas.get(i).loadTo(entities.get(i));
+		}
+	}
+
+	@Override
+	public void undo () {
+		entities.forEach(EntityProxy::reload);
+		for (int i = 0; i < entities.size; i++) {
+			oldDatas.get(i).loadTo(entities.get(i));
+		}
+	}
+
+	private static class EntityPositionData {
+		public float x;
+		public float y;
+
+		public void saveFrom (EntityProxy entity) {
+			x = entity.getX();
+			y = entity.getY();
+		}
+
+		public void loadTo (EntityProxy entity) {
+			entity.setPosition(x, y);
+		}
+	}
+
+}
+
