3701a6452a2c70c3b175af6bfbe0fe184342300e
Paweł Pastuszak <kotcrab@gmail.com>
2015-08-14 20:57:50 +0200

    Prepare for next version dev

--- Editor/src/com/kotcrab/vis/editor/App.java
+++ Editor/src/com/kotcrab/vis/editor/App.java
@@ -38,9 +38,9 @@ import java.util.jar.Manifest;
 public class App {
 	public static final String TAG = "App";
 
-	public static final int VERSION_CODE = 7;
+	public static final int VERSION_CODE = 8;
 	public static final int PLUGIN_COMPATIBILITY_CODE = 2;
-	public static final String VERSION = "0.2.2";
+	public static final String VERSION = "0.2.3-SNAPSHOT";
 
 	public static boolean buildTimestampValid = false;
 	public static String buildTimestamp;
