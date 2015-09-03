a4a73e960326282bdf83c3951f5cafb47770b047
Paweł Pastuszak <kotcrab@gmail.com>
2015-09-03 21:54:29 +0200

    Add ColumnGroup

---
+++ UI/src/com/kotcrab/vis/ui/layout/ColumnGroup.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2014-2015 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.kotcrab.vis.ui.layout;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.ui.WidgetGroup;
+import com.badlogic.gdx.scenes.scene2d.utils.Layout;
+import com.badlogic.gdx.utils.SnapshotArray;
+
+/**
+ * ColumnGroup Lays it's children in single column. Each child is added in new row.
+ * @author Kotcrab
+ * @since 0.8.2
+ */
+public class ColumnGroup extends WidgetGroup {
+	private float prefWidth;
+	private float prefHeight;
+	private float lastPrefHeight;
+	private boolean sizeInvalid = true;
+
+	public ColumnGroup () {
+		setTouchable(Touchable.childrenOnly);
+	}
+
+	private void computeSize () {
+		prefWidth = 0;
+		prefHeight = 0;
+		sizeInvalid = false;
+		SnapshotArray<Actor> children = getChildren();
+
+		for (int i = 0, n = children.size; i < n; i++) {
+			Actor child = children.get(i);
+			float width = child.getWidth();
+			float height = child.getHeight();
+			if (child instanceof Layout) {
+				Layout layout = (Layout) child;
+				width = layout.getPrefWidth();
+				height = layout.getPrefHeight();
+			}
+
+			prefWidth = Math.max(prefWidth, width);
+			prefHeight += height;
+		}
+	}
+
+	@Override
+	public void layout () {
+		if (sizeInvalid) {
+			computeSize();
+			if (lastPrefHeight != prefHeight) {
+				lastPrefHeight = prefHeight;
+				invalidateHierarchy();
+			}
+		}
+
+		float y = getHeight();
+
+		SnapshotArray<Actor> children = getChildren();
+		for (int i = 0, n = children.size; i < n; i++) {
+			Actor child = children.get(i);
+			float width = child.getWidth();
+			float height = child.getHeight();
+			if (child instanceof Layout) {
+				Layout layout = (Layout) child;
+				width = layout.getPrefWidth();
+				height = layout.getPrefHeight();
+			}
+
+			child.setBounds(0, y - height, width, height);
+			y -= height;
+		}
+	}
+
+	@Override
+	public void invalidate () {
+		super.invalidate();
+		sizeInvalid = true;
+	}
+
+	@Override
+	public float getPrefWidth () {
+		if (sizeInvalid) computeSize();
+		return prefWidth;
+	}
+
+	@Override
+	public float getPrefHeight () {
+		if (sizeInvalid) computeSize();
+		return prefHeight;
+	}
+}
