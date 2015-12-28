f083d5d0d94decee5c521dff77c0b76f7fe34204
Paweł Pastuszak <kotcrab@gmail.com>
2015-12-28 12:48:53 +0100

    Split EntityProperties, fix ComponentPanel missing growX

---
+++ Editor/src/com/kotcrab/vis/editor/ui/scene/entityproperties/BasicEntityPropertiesTable.java
@@ -0,0 +1,292 @@
+/*
+ * Copyright 2014-2015 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.kotcrab.vis.editor.ui.scene.entityproperties;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.kotcrab.vis.editor.proxy.EntityProxy;
+import com.kotcrab.vis.editor.ui.TintImage;
+import com.kotcrab.vis.editor.util.scene2d.FieldUtils;
+import com.kotcrab.vis.editor.util.value.FloatProxyValue;
+import com.kotcrab.vis.editor.util.vis.EntityUtils;
+import com.kotcrab.vis.runtime.util.ImmutableArray;
+import com.kotcrab.vis.ui.util.TableUtils;
+import com.kotcrab.vis.ui.util.value.VisValue;
+import com.kotcrab.vis.ui.widget.VisLabel;
+import com.kotcrab.vis.ui.widget.VisTable;
+import com.kotcrab.vis.ui.widget.VisValidatableTextField;
+import com.kotcrab.vis.ui.widget.color.ColorPicker;
+import com.kotcrab.vis.ui.widget.color.ColorPickerAdapter;
+import com.kotcrab.vis.ui.widget.color.ColorPickerListener;
+
+import static com.kotcrab.vis.editor.ui.scene.entityproperties.EntityProperties.*;
+
+/** @author Kotcrab */
+public class BasicEntityPropertiesTable extends VisTable {
+	private final EntityProperties properties;
+	private final ColorPicker picker;
+
+	private ColorPickerListener pickerListener;
+	private TintImage tint;
+
+	private VisTable idTable;
+
+	private VisTable positionTable;
+	private VisTable scaleTable;
+	private VisTable originTable;
+	private VisTable rotationTable;
+	private VisTable tintTable;
+	private VisTable flipTable;
+
+	private VisValidatableTextField idField;
+	private NumberInputField xField;
+	private NumberInputField yField;
+	private NumberInputField xScaleField;
+	private NumberInputField yScaleField;
+	private NumberInputField xOriginField;
+	private NumberInputField yOriginField;
+	private NumberInputField rotationField;
+	private IndeterminateCheckbox xFlipCheck;
+	private IndeterminateCheckbox yFlipCheck;
+
+	public BasicEntityPropertiesTable (EntityProperties properties, ColorPicker picker) {
+		this.properties = properties;
+		this.picker = picker;
+
+		createIdTable();
+		createPositionTable();
+		createScaleTable();
+		createOriginTable();
+		createRotationTintTable();
+		createFlipTable();
+
+		pickerListener = new ColorPickerAdapter() {
+			@Override
+			public void finished (Color newColor) {
+				for (EntityProxy entity : properties.getSelectedEntities())
+					entity.setColor(newColor);
+
+				properties.getParentTab().dirty();
+				tint.setColor(newColor);
+				tint.setUnknown(false);
+				picker.setListener(null);
+				properties.endSnapshot();
+			}
+		};
+	}
+
+	private void createIdTable () {
+		idTable = new VisTable(true);
+		idTable.add(new VisLabel("ID"));
+		idTable.add(idField = new VisValidatableTextField()).expandX().fillX();
+
+		properties.setupStdPropertiesTextField(idField);
+	}
+
+	private void createPositionTable () {
+		positionTable = new VisTable(true);
+		positionTable.add(new VisLabel("Position")).width(LABEL_WIDTH);
+		positionTable.add(new VisLabel("X")).width(AXIS_LABEL_WIDTH);
+		positionTable.add(xField = properties.createNewNumberField()).width(FIELD_WIDTH);
+		positionTable.add(new VisLabel("Y")).width(AXIS_LABEL_WIDTH);
+		positionTable.add(yField = properties.createNewNumberField()).width(FIELD_WIDTH);
+	}
+
+	private void createScaleTable () {
+		scaleTable = new VisTable(true);
+		scaleTable.add(new VisLabel("Scale")).width(LABEL_WIDTH);
+		scaleTable.add(new VisLabel("X")).width(AXIS_LABEL_WIDTH);
+		scaleTable.add(xScaleField = properties.createNewNumberField()).width(FIELD_WIDTH);
+		scaleTable.add(new VisLabel("Y")).width(AXIS_LABEL_WIDTH);
+		scaleTable.add(yScaleField = properties.createNewNumberField()).width(FIELD_WIDTH);
+	}
+
+	private void createOriginTable () {
+		originTable = new VisTable(true);
+		originTable.add(new VisLabel("Origin")).width(LABEL_WIDTH);
+		originTable.add(new VisLabel("X")).width(AXIS_LABEL_WIDTH);
+		originTable.add(xOriginField = properties.createNewNumberField()).width(FIELD_WIDTH);
+		originTable.add(new VisLabel("Y")).width(AXIS_LABEL_WIDTH);
+		originTable.add(yOriginField = properties.createNewNumberField()).width(FIELD_WIDTH);
+	}
+
+	private void createRotationTintTable () {
+		tint = new TintImage();
+		tint.addListener(new ClickListener() {
+			@Override
+			public void clicked (InputEvent event, float x, float y) {
+				properties.beginSnapshot();
+				picker.setColor(tint.getColor());
+				picker.setListener(pickerListener);
+				getStage().addActor(picker.fadeIn());
+			}
+		});
+
+		tintTable = new VisTable(true);
+		tintTable.add(new VisLabel("Tint"));
+		tintTable.add(tint).size(20);
+
+		rotationTable = new VisTable(true);
+		rotationTable.add(new VisLabel("Rotation")).width(LABEL_WIDTH);
+		rotationTable.add(new VisLabel(" ")).width(AXIS_LABEL_WIDTH);
+		rotationTable.add(rotationField = properties.createNewNumberField()).width(FIELD_WIDTH);
+	}
+
+	private void createFlipTable () {
+		flipTable = new VisTable(true);
+
+		flipTable.add(new VisLabel("Flip"));
+		flipTable.add(xFlipCheck = new IndeterminateCheckbox("X"));
+		flipTable.add(yFlipCheck = new IndeterminateCheckbox("Y"));
+
+		xFlipCheck.addListener(properties.getSharedCheckBoxChangeListener());
+		yFlipCheck.addListener(properties.getSharedCheckBoxChangeListener());
+	}
+
+	public void rebuildPropertiesTable () {
+		ImmutableArray<EntityProxy> entities = properties.getSelectedEntities();
+
+		VisTable rotationTintTable = new VisTable(true);
+		if (EntityUtils.isRotationSupportedForEntities(entities)) {
+			rotationTintTable.add(rotationTable);
+		}
+		rotationTintTable.add().expand().fill();
+		if (EntityUtils.isTintSupportedForEntities(entities)) {
+			rotationTintTable.add(tintTable);
+		}
+
+		reset();
+		TableUtils.setSpacingDefaults(this);
+		defaults().padRight(0).fillX();
+		add(idTable).row();
+		add(positionTable).row();
+
+		if (EntityUtils.isScaleSupportedForEntities(entities)) {
+			add(scaleTable).row();
+		}
+
+		if (EntityUtils.isOriginSupportedForEntities(entities)) {
+			add(originTable).row();
+		}
+
+		if (EntityUtils.isRotationSupportedForEntities(entities) || EntityUtils.isTintSupportedForEntities(entities)) {
+			add(rotationTintTable).maxWidth(new VisValue(context -> positionTable.getPrefWidth())).row();
+		}
+
+		if (EntityUtils.isFlipSupportedForEntities(entities)) {
+			add(flipTable).right().fill(false).spaceBottom(2).row();
+		}
+	}
+
+	public void setValuesToEntity () {
+		ImmutableArray<EntityProxy> entities = properties.getSelectedEntities();
+
+		for (int i = 0; i < entities.size(); i++) {
+			EntityProxy entity = entities.get(i);
+
+			//TODO support indeterminate textfield
+			if (properties.isGroupSelected() == false && idField.getText().equals("<?>") == false)
+				entity.setId(idField.getText().equals("") ? null : idField.getText());
+
+			entity.setPosition(FieldUtils.getFloat(xField, entity.getX()), FieldUtils.getFloat(yField, entity.getY()));
+
+			if (EntityUtils.isScaleSupportedForEntities(entities))
+				entity.setScale(FieldUtils.getFloat(xScaleField, entity.getScaleX()), FieldUtils.getFloat(yScaleField, entity.getScaleY()));
+
+			if (EntityUtils.isOriginSupportedForEntities(entities))
+				entity.setOrigin(FieldUtils.getFloat(xOriginField, entity.getOriginX()), FieldUtils.getFloat(yOriginField, entity.getOriginY()));
+
+			if (EntityUtils.isRotationSupportedForEntities(entities))
+				entity.setRotation(FieldUtils.getFloat(rotationField, entity.getRotation()));
+
+			if (EntityUtils.isFlipSupportedForEntities(entities)) {
+				if (xFlipCheck.isIndeterminate() == false)
+					entity.setFlip(xFlipCheck.isChecked(), entity.isFlipY());
+
+				if (yFlipCheck.isIndeterminate() == false)
+					entity.setFlip(entity.isFlipX(), yFlipCheck.isChecked());
+			}
+		}
+	}
+
+	public void updateUIValues (boolean updateInvalidFields) {
+		ImmutableArray<EntityProxy> entities = properties.getSelectedEntities();
+
+		if (properties.isGroupSelected()) {
+			idField.setText("<id cannot be set for group>");
+			idField.setDisabled(true);
+		} else {
+			idField.setText(EntityUtils.getCommonId(entities));
+			idField.setDisabled(false);
+		}
+
+		xField.setText(getEntitiesFieldFloatValue(EntityProxy::getX));
+		yField.setText(getEntitiesFieldFloatValue(EntityProxy::getY));
+
+		if (EntityUtils.isScaleSupportedForEntities(entities)) {
+			if (updateInvalidFields || xScaleField.isInputValid())
+				xScaleField.setText(getEntitiesFieldFloatValue(EntityProxy::getScaleX));
+
+			if (updateInvalidFields || yScaleField.isInputValid())
+				yScaleField.setText(getEntitiesFieldFloatValue(EntityProxy::getScaleY));
+		}
+
+		if (EntityUtils.isOriginSupportedForEntities(entities)) {
+			if (updateInvalidFields || xOriginField.isInputValid())
+				xOriginField.setText(getEntitiesFieldFloatValue(EntityProxy::getOriginX));
+
+			if (updateInvalidFields || yOriginField.isInputValid())
+				yOriginField.setText(getEntitiesFieldFloatValue(EntityProxy::getOriginY));
+		}
+
+		if (EntityUtils.isRotationSupportedForEntities(entities)) {
+			if (updateInvalidFields || rotationField.isInputValid())
+				rotationField.setText(getEntitiesFieldFloatValue(EntityProxy::getRotation));
+		}
+
+		if (EntityUtils.isTintSupportedForEntities(entities)) {
+			setTintUIForEntities();
+		}
+
+		if (EntityUtils.isFlipSupportedForEntities(entities)) {
+			EntityUtils.setCommonCheckBoxState(entities, xFlipCheck, EntityProxy::isFlipX);
+			EntityUtils.setCommonCheckBoxState(entities, yFlipCheck, EntityProxy::isFlipY);
+		}
+	}
+
+	private String getEntitiesFieldFloatValue (FloatProxyValue floatProxyValue) {
+		ImmutableArray<EntityProxy> entities = properties.getSelectedEntities();
+		return EntityUtils.getEntitiesCommonFloatValue(entities, floatProxyValue);
+	}
+
+	private void setTintUIForEntities () {
+		ImmutableArray<EntityProxy> entities = properties.getSelectedEntities();
+
+		Color firstColor = entities.first().getColor();
+		for (EntityProxy entity : entities) {
+			if (!firstColor.equals(entity.getColor())) {
+				tint.setUnknown(true);
+				return;
+			}
+		}
+
+		tint.setUnknown(false);
+		tint.setColor(firstColor);
+	}
+
+}
--- Editor/src/com/kotcrab/vis/editor/ui/scene/entityproperties/EntityProperties.java
+++ Editor/src/com/kotcrab/vis/editor/ui/scene/entityproperties/EntityProperties.java
@@ -21,14 +21,12 @@ import com.artemis.Entity;
 import com.artemis.utils.Bag;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.FocusListener;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Array.ArrayIterable;
@@ -38,10 +36,6 @@ import com.kotcrab.vis.editor.entity.EntityScheme;
 import com.kotcrab.vis.editor.entity.EntityScheme.CloningPolicy;
 import com.kotcrab.vis.editor.entity.EntityScheme.UUIDPolicy;
 import com.kotcrab.vis.editor.module.editor.*;
-import com.kotcrab.vis.editor.module.project.FileAccessModule;
-import com.kotcrab.vis.editor.module.project.FontCacheModule;
-import com.kotcrab.vis.editor.module.project.SceneIOModule;
-import com.kotcrab.vis.editor.module.scene.CameraModule;
 import com.kotcrab.vis.editor.module.scene.SceneModuleContainer;
 import com.kotcrab.vis.editor.module.scene.UndoModule;
 import com.kotcrab.vis.editor.module.scene.action.ComponentAddAction;
@@ -52,7 +46,6 @@ import com.kotcrab.vis.editor.module.scene.system.VisComponentManipulator;
 import com.kotcrab.vis.editor.plugin.EditorEntitySupport;
 import com.kotcrab.vis.editor.plugin.api.ComponentTableProvider;
 import com.kotcrab.vis.editor.proxy.EntityProxy;
-import com.kotcrab.vis.editor.ui.TintImage;
 import com.kotcrab.vis.editor.ui.scene.entityproperties.specifictable.BMPTextUITable;
 import com.kotcrab.vis.editor.ui.scene.entityproperties.specifictable.GroupUITable;
 import com.kotcrab.vis.editor.ui.scene.entityproperties.specifictable.SpecificUITable;
@@ -60,21 +53,15 @@ import com.kotcrab.vis.editor.ui.scene.entityproperties.specifictable.TtfTextUIT
 import com.kotcrab.vis.editor.ui.toast.DetailsToast;
 import com.kotcrab.vis.editor.util.gdx.ArrayUtils;
 import com.kotcrab.vis.editor.util.scene2d.EventStopper;
-import com.kotcrab.vis.editor.util.scene2d.FieldUtils;
 import com.kotcrab.vis.editor.util.scene2d.VisChangeListener;
 import com.kotcrab.vis.editor.util.undo.MonoUndoableActionGroup;
 import com.kotcrab.vis.editor.util.undo.UndoableAction;
-import com.kotcrab.vis.editor.util.value.FloatProxyValue;
 import com.kotcrab.vis.editor.util.vis.EntityUtils;
 import com.kotcrab.vis.runtime.util.ImmutableArray;
 import com.kotcrab.vis.ui.VisUI;
 import com.kotcrab.vis.ui.util.ActorUtils;
 import com.kotcrab.vis.ui.util.TableUtils;
-import com.kotcrab.vis.ui.util.value.VisValue;
 import com.kotcrab.vis.ui.widget.*;
-import com.kotcrab.vis.ui.widget.color.ColorPicker;
-import com.kotcrab.vis.ui.widget.color.ColorPickerAdapter;
-import com.kotcrab.vis.ui.widget.color.ColorPickerListener;
 import com.kotcrab.vis.ui.widget.tabbedpane.Tab;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 
@@ -100,17 +87,11 @@ public class EntityProperties extends VisTable {
 	private ExtensionStorageModule extensionStorage;
 	private ClonerModule cloner;
 
-	private FileAccessModule fileAccessModule;
-	private FontCacheModule fontCacheModule;
-	private SceneIOModule sceneIO;
-
 	private UndoModule undoModule;
-	private CameraModule cameraModule;
 	private EntityManipulatorModule entityManipulator;
 
 	private VisComponentManipulator componentManipulator;
 
-	private ColorPicker picker;
 	private Tab parentTab;
 
 	private boolean groupSelected;
@@ -120,9 +101,6 @@ public class EntityProperties extends VisTable {
 	private FocusListener sharedFocusListener;
 	private InputListener sharedInputListener;
 
-	private ColorPickerListener pickerListener;
-	private TintImage tint;
-
 	private boolean snapshotInProgress;
 	private SnapshotUndoableActionGroup snapshots;
 
@@ -131,15 +109,7 @@ public class EntityProperties extends VisTable {
 
 	//UI
 	private VisTable propertiesTable;
-
-	private VisTable idTable;
-
-	private VisTable positionTable;
-	private VisTable scaleTable;
-	private VisTable originTable;
-	private VisTable rotationTable;
-	private VisTable tintTable;
-	private VisTable flipTable;
+	private BasicEntityPropertiesTable basicProperties;
 
 	private ComponentSelectDialog componentSelectDialog;
 	private VisTextButton addComponentButton;
@@ -150,16 +120,6 @@ public class EntityProperties extends VisTable {
 	private Array<ComponentTable<?>> componentTables = new Array<>();
 	private Array<ComponentTable<?>> activeComponentTables = new Array<>();
 
-	private VisValidatableTextField idField;
-	private NumberInputField xField;
-	private NumberInputField yField;
-	private NumberInputField xScaleField;
-	private NumberInputField yScaleField;
-	private NumberInputField xOriginField;
-	private NumberInputField yOriginField;
-	private NumberInputField rotationField;
-	private IndeterminateCheckbox xFlipCheck;
-	private IndeterminateCheckbox yFlipCheck;
 	private SceneModuleContainer sceneMC;
 
 	public EntityProperties (SceneModuleContainer sceneMC, Tab parentSceneTab) {
@@ -168,7 +128,6 @@ public class EntityProperties extends VisTable {
 
 		this.sceneMC = sceneMC;
 		this.parentTab = parentSceneTab;
-		this.picker = colorPickerModule.getPicker();
 
 		setBackground(VisUI.getSkin().getDrawable("window-bg"));
 		setTouchable(Touchable.childrenOnly);
@@ -182,7 +141,7 @@ public class EntityProperties extends VisTable {
 				if (actor instanceof VisSelectBox)
 					throw new IllegalStateException("sharedChangeListener cannot be used for selectBoxes, use sharedSelectBoxChangeListener instead");
 
-				setValuesToEntity();
+				setValuesToEntities();
 				parentTab.dirty();
 			}
 		};
@@ -191,7 +150,7 @@ public class EntityProperties extends VisTable {
 			@Override
 			public void changed (ChangeEvent event, Actor actor) {
 				beginSnapshot();
-				setValuesToEntity();
+				setValuesToEntities();
 				parentTab.dirty();
 				endSnapshot();
 			}
@@ -212,7 +171,7 @@ public class EntityProperties extends VisTable {
 			public boolean keyDown (InputEvent event, int keycode) {
 				if (keycode == Keys.ENTER) {
 					if (snapshotInProgress == false) beginSnapshot();
-					setValuesToEntity();
+					setValuesToEntities();
 					parentTab.dirty();
 					endSnapshot();
 					return true;
@@ -222,26 +181,7 @@ public class EntityProperties extends VisTable {
 			}
 		};
 
-		pickerListener = new ColorPickerAdapter() {
-			@Override
-			public void finished (Color newColor) {
-				for (EntityProxy entity : entityManipulator.getSelectedEntities())
-					entity.setColor(newColor);
-
-				parentTab.dirty();
-				tint.setColor(newColor);
-				tint.setUnknown(false);
-				picker.setListener(null);
-				endSnapshot();
-			}
-		};
-
-		createIdTable();
-		createPositionTable();
-		createScaleTable();
-		createOriginTable();
-		createRotationTintTable();
-		createFlipTable();
+		basicProperties = new BasicEntityPropertiesTable(this, colorPickerModule.getPicker());
 
 		componentSelectDialog = new ComponentSelectDialog(sceneMC, this, clazz -> {
 			try {
@@ -293,97 +233,14 @@ public class EntityProperties extends VisTable {
 		pack();
 	}
 
-	private void createIdTable () {
-		idTable = new VisTable(true);
-		idTable.add(new VisLabel("ID"));
-		idTable.add(idField = new VisValidatableTextField()).expandX().fillX();
-		idField.setProgrammaticChangeEvents(false);
-
-		idField.addListener(sharedChangeListener);
-		idField.addListener(sharedFocusListener);
-		idField.addListener(sharedInputListener);
-	}
-
-	private void createPositionTable () {
-		positionTable = new VisTable(true);
-		positionTable.add(new VisLabel("Position")).width(LABEL_WIDTH);
-		positionTable.add(new VisLabel("X")).width(AXIS_LABEL_WIDTH);
-		positionTable.add(xField = createNewNumberField()).width(FIELD_WIDTH);
-		positionTable.add(new VisLabel("Y")).width(AXIS_LABEL_WIDTH);
-		positionTable.add(yField = createNewNumberField()).width(FIELD_WIDTH);
-	}
-
-	private void createScaleTable () {
-		scaleTable = new VisTable(true);
-		scaleTable.add(new VisLabel("Scale")).width(LABEL_WIDTH);
-		scaleTable.add(new VisLabel("X")).width(AXIS_LABEL_WIDTH);
-		scaleTable.add(xScaleField = createNewNumberField()).width(FIELD_WIDTH);
-		scaleTable.add(new VisLabel("Y")).width(AXIS_LABEL_WIDTH);
-		scaleTable.add(yScaleField = createNewNumberField()).width(FIELD_WIDTH);
-	}
-
-	private void createOriginTable () {
-		originTable = new VisTable(true);
-		originTable.add(new VisLabel("Origin")).width(LABEL_WIDTH);
-		originTable.add(new VisLabel("X")).width(AXIS_LABEL_WIDTH);
-		originTable.add(xOriginField = createNewNumberField()).width(FIELD_WIDTH);
-		originTable.add(new VisLabel("Y")).width(AXIS_LABEL_WIDTH);
-		originTable.add(yOriginField = createNewNumberField()).width(FIELD_WIDTH);
-	}
-
-	private void createRotationTintTable () {
-		tint = new TintImage();
-		tint.addListener(new ClickListener() {
-			@Override
-			public void clicked (InputEvent event, float x, float y) {
-				beginSnapshot();
-				picker.setColor(tint.getColor());
-				picker.setListener(pickerListener);
-				getStage().addActor(picker.fadeIn());
-			}
-		});
-
-		tintTable = new VisTable(true);
-		tintTable.add(new VisLabel("Tint"));
-		tintTable.add(tint).size(20);
-
-		rotationTable = new VisTable(true);
-		rotationTable.add(new VisLabel("Rotation")).width(LABEL_WIDTH);
-		rotationTable.add(new VisLabel(" ")).width(AXIS_LABEL_WIDTH);
-		rotationTable.add(rotationField = createNewNumberField()).width(FIELD_WIDTH);
-	}
-
-	private void createFlipTable () {
-		flipTable = new VisTable(true);
-
-		flipTable.add(new VisLabel("Flip"));
-		flipTable.add(xFlipCheck = new IndeterminateCheckbox("X"));
-		flipTable.add(yFlipCheck = new IndeterminateCheckbox("Y"));
-
-		xFlipCheck.addListener(sharedChckAndSelectBoxChangeListener);
-		yFlipCheck.addListener(sharedChckAndSelectBoxChangeListener);
-	}
-
 	private void rebuildPropertiesTable () {
 		propertiesTable.reset();
 		TableUtils.setSpacingDefaults(propertiesTable);
 
 		ImmutableArray<EntityProxy> entities = entityManipulator.getSelectedEntities();
 
-		VisTable rotationTintTable = new VisTable(true);
-		if (EntityUtils.isRotationSupportedForEntities(entities)) rotationTintTable.add(rotationTable);
-		rotationTintTable.add().expand().fill();
-		if (EntityUtils.isTintSupportedForEntities(entities)) rotationTintTable.add(tintTable);
-
-		propertiesTable.defaults().padRight(0).fillX();
-		propertiesTable.add(idTable).row();
-		propertiesTable.add(positionTable).row();
-		if (EntityUtils.isScaleSupportedForEntities(entities)) propertiesTable.add(scaleTable).row();
-		if (EntityUtils.isOriginSupportedForEntities(entities)) propertiesTable.add(originTable).row();
-		if (EntityUtils.isRotationSupportedForEntities(entities) || EntityUtils.isTintSupportedForEntities(entities))
-			propertiesTable.add(rotationTintTable).maxWidth(new VisValue(context -> positionTable.getPrefWidth())).row();
-		if (EntityUtils.isFlipSupportedForEntities(entities))
-			propertiesTable.add(flipTable).right().fill(false).spaceBottom(2).row();
+		basicProperties.rebuildPropertiesTable();
+		propertiesTable.add(basicProperties).row();
 
 		activeSpecificTable = null;
 		for (SpecificUITable table : specificTables) {
@@ -407,7 +264,7 @@ public class EntityProperties extends VisTable {
 
 					if (componentTable != null) {
 						activeComponentTables.add(componentTable);
-						propertiesTable.add(new ComponentPanel(sceneMC, componentManipulator, component.getClass().getSimpleName(), componentTable));
+						propertiesTable.add(new ComponentPanel(sceneMC, componentManipulator, component.getClass().getSimpleName(), componentTable)).growX();
 						propertiesTable.row();
 					}
 				}
@@ -538,55 +395,8 @@ public class EntityProperties extends VisTable {
 		return parentTab;
 	}
 
-	private void setTintUIForEntities () {
-		ImmutableArray<EntityProxy> entities = entityManipulator.getSelectedEntities();
-
-		Color firstColor = entities.first().getColor();
-		for (EntityProxy entity : entities) {
-			if (!firstColor.equals(entity.getColor())) {
-				tint.setUnknown(true);
-				return;
-			}
-		}
-
-		tint.setUnknown(false);
-		tint.setColor(firstColor);
-	}
-
-	private String getEntitiesFieldFloatValue (FloatProxyValue floatProxyValue) {
-		ImmutableArray<EntityProxy> entities = entityManipulator.getSelectedEntities();
-		return EntityUtils.getEntitiesCommonFloatValue(entities, floatProxyValue);
-	}
-
-	private void setValuesToEntity () {
-		ImmutableArray<EntityProxy> entities = entityManipulator.getSelectedEntities();
-
-		for (int i = 0; i < entities.size(); i++) {
-			EntityProxy entity = entities.get(i);
-
-			//TODO support indeterminate textfield
-			if (groupSelected == false && idField.getText().equals("<?>") == false)
-				entity.setId(idField.getText().equals("") ? null : idField.getText());
-
-			entity.setPosition(FieldUtils.getFloat(xField, entity.getX()), FieldUtils.getFloat(yField, entity.getY()));
-
-			if (EntityUtils.isScaleSupportedForEntities(entities))
-				entity.setScale(FieldUtils.getFloat(xScaleField, entity.getScaleX()), FieldUtils.getFloat(yScaleField, entity.getScaleY()));
-
-			if (EntityUtils.isOriginSupportedForEntities(entities))
-				entity.setOrigin(FieldUtils.getFloat(xOriginField, entity.getOriginX()), FieldUtils.getFloat(yOriginField, entity.getOriginY()));
-
-			if (EntityUtils.isRotationSupportedForEntities(entities))
-				entity.setRotation(FieldUtils.getFloat(rotationField, entity.getRotation()));
-
-			if (EntityUtils.isFlipSupportedForEntities(entities)) {
-				if (xFlipCheck.isIndeterminate() == false)
-					entity.setFlip(xFlipCheck.isChecked(), entity.isFlipY());
-
-				if (yFlipCheck.isIndeterminate() == false)
-					entity.setFlip(entity.isFlipX(), yFlipCheck.isChecked());
-			}
-		}
+	private void setValuesToEntities () {
+		basicProperties.setValuesToEntity();
 
 		if (activeSpecificTable != null) activeSpecificTable.setValuesToEntities();
 		for (ComponentTable<?> table : new ArrayIterable<>(activeComponentTables))
@@ -603,7 +413,7 @@ public class EntityProperties extends VisTable {
 		} else {
 			setVisible(true);
 
-			updateBasicValues(updateInvalidFields);
+			basicProperties.updateUIValues(updateInvalidFields);
 
 			uiValuesUpdateInProgress = true;
 
@@ -622,49 +432,9 @@ public class EntityProperties extends VisTable {
 		}
 	}
 
-	private void updateBasicValues (boolean updateInvalidFields) {
-		ImmutableArray<EntityProxy> entities = entityManipulator.getSelectedEntities();
 
-		if (groupSelected) {
-			idField.setText("<id cannot be set for group>");
-			idField.setDisabled(true);
-		} else {
-			idField.setText(EntityUtils.getCommonId(entities));
-			idField.setDisabled(false);
-		}
-
-		xField.setText(getEntitiesFieldFloatValue(EntityProxy::getX));
-		yField.setText(getEntitiesFieldFloatValue(EntityProxy::getY));
-
-		if (EntityUtils.isScaleSupportedForEntities(entities)) {
-			if (updateInvalidFields || xScaleField.isInputValid())
-				xScaleField.setText(getEntitiesFieldFloatValue(EntityProxy::getScaleX));
-
-			if (updateInvalidFields || yScaleField.isInputValid())
-				yScaleField.setText(getEntitiesFieldFloatValue(EntityProxy::getScaleY));
-		}
-
-		if (EntityUtils.isOriginSupportedForEntities(entities)) {
-			if (updateInvalidFields || xOriginField.isInputValid())
-				xOriginField.setText(getEntitiesFieldFloatValue(EntityProxy::getOriginX));
-
-			if (updateInvalidFields || yOriginField.isInputValid())
-				yOriginField.setText(getEntitiesFieldFloatValue(EntityProxy::getOriginY));
-		}
-
-		if (EntityUtils.isRotationSupportedForEntities(entities)) {
-			if (updateInvalidFields || rotationField.isInputValid())
-				rotationField.setText(getEntitiesFieldFloatValue(EntityProxy::getRotation));
-		}
-
-		if (EntityUtils.isTintSupportedForEntities(entities)) {
-			setTintUIForEntities();
-		}
-
-		if (EntityUtils.isFlipSupportedForEntities(entities)) {
-			EntityUtils.setCommonCheckBoxState(entities, xFlipCheck, EntityProxy::isFlipX);
-			EntityUtils.setCommonCheckBoxState(entities, yFlipCheck, EntityProxy::isFlipY);
-		}
+	public boolean isGroupSelected () {
+		return groupSelected;
 	}
 
 	public void loadSupportsSpecificTables () {
