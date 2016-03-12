e2918b676d0c2a3ad6b2da67146db3380ea95335
Paweł Pastuszak <kotcrab@gmail.com>
2016-03-12 15:53:54 +0100

    Fix UpdateChecker

--- editor/src/com/kotcrab/vis/editor/module/editor/UpdateCheckerModule.java
+++ editor/src/com/kotcrab/vis/editor/module/editor/UpdateCheckerModule.java
@@ -119,10 +119,10 @@ public class UpdateCheckerModule extends EditorModule {
 				for (int i = 1; i < links.size(); i++) { //first link is ../ so we skip it
 					String url = links.get(i).absUrl("href");
 					Matcher matcher = BUILD_TIMESTAMP_PATTERN.matcher(url);
-					matcher.find();
-					String timestamp = matcher.group();
-
-					builds.add(new EditorBuild(timestamp, url));
+					if(matcher.find()) {
+						String timestamp = matcher.group();
+						builds.add(new EditorBuild(timestamp, url));
+					}
 				}
 
 				listener.result(builds);
