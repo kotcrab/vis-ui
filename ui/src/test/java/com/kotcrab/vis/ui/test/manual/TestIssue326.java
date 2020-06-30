fb9f97b298be31cdf4afa404bd52856b5a5b23dd
fgnm <fgnm.dev@gmail.com>
2020-06-30 17:45:27 +0200

    Format code properly

--- ui/src/test/java/com/kotcrab/vis/ui/test/manual/TestIssue326.java
+++ ui/src/test/java/com/kotcrab/vis/ui/test/manual/TestIssue326.java
@@ -9,30 +9,30 @@ import com.kotcrab.vis.ui.widget.spinner.IntSpinnerModel;
 import com.kotcrab.vis.ui.widget.spinner.Spinner;
 
 public class TestIssue326 extends VisWindow {
-    public TestIssue326 () {
-        super("issue #326");
+	public TestIssue326 () {
+		super("issue #326");
 
-        final VisTextField passwordTextField = new VisTextField("password");
+		final VisTextField passwordTextField = new VisTextField("password");
 
-        final IntSpinnerModel intModel = new IntSpinnerModel(10, -5, 20, 2);
-        Spinner intSpinner = new Spinner("int", intModel);
-        intSpinner.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                passwordTextField.setDisabled(true);
-            }
-        });
+		final IntSpinnerModel intModel = new IntSpinnerModel(10, -5, 20, 2);
+		Spinner intSpinner = new Spinner("int", intModel);
+		intSpinner.addListener(new ChangeListener() {
+			@Override
+			public void changed (ChangeEvent event, Actor actor) {
+				passwordTextField.setDisabled(true);
+			}
+		});
 
-        add(new LinkLabel("issue #326 - FocusManager crash", "https://github.com/kotcrab/vis-ui/issues/326")).colspan(2).row();
-        add(passwordTextField);
-        row();
-        add(intSpinner);
+		add(new LinkLabel("issue #326 - FocusManager crash", "https://github.com/kotcrab/vis-ui/issues/326")).colspan(2).row();
+		add(passwordTextField);
+		row();
+		add(intSpinner);
 
-        setResizable(false);
-        setModal(false);
-        addCloseButton();
-        closeOnEscape();
-        pack();
-        centerWindow();
-    }
+		setResizable(false);
+		setModal(false);
+		addCloseButton();
+		closeOnEscape();
+		pack();
+		centerWindow();
+	}
 }
